96d1ea681b3cdb224062e810b5f1cb1a0f6e0b5e
anil <anilkothattil@gmail.com>
2018-04-03 13:38:21 +0530

    udpserver started

---
+++ UDPServer.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.net.*;
+ 
+class UDPServer
+{
+   public static void main(String args[]) throws Exception
+      {
+         DatagramSocket serverSocket = new DatagramSocket(9876);
+            byte[] receiveData = new byte[1024];
+            byte[] sendData = new byte[1024];
+           
+      }
+}
