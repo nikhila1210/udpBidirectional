c7bb0d5530d5230bff8df2ca32f5b78b9c1f5d44
anilagraceprakash <anilagracep@gmail.com>
2018-04-03 14:17:07 +0530

    Client file modified

--- UDPClient.java
+++ UDPClient.java
@@ -10,3 +10,9 @@ class UDPClient
       byte[] sendData = new byte[1024];
       byte[] receiveData = new byte[1024];
       boolean flag=true;
+      while(flag)
+       {
+      String sentence = inFromUser.readLine();
+      sendData = sentence.getBytes();
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+      clientSocket.send(sendPacket);
