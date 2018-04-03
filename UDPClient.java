a2d34a2d951033283570988ea3baefa35d95b775
anilagraceprakash <anilagracep@gmail.com>
2018-04-03 14:13:45 +0530

    Client file modified

--- UDPClient.java
+++ UDPClient.java
@@ -4,3 +4,9 @@ class UDPClient
 {
    public static void main(String args[]) throws Exception
    {
+      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+      DatagramSocket clientSocket = new DatagramSocket();
+      InetAddress IPAddress = InetAddress.getByName("localhost");
+      byte[] sendData = new byte[1024];
+      byte[] receiveData = new byte[1024];
+      boolean flag=true;
